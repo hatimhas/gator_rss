a5b697b94ae4b97964b95f16040d00048666761b
Hatim <hatimhasbullah@gmail.com>
2025-05-25 15:27:54 +0800

    added full login functionalities with structs

--- main.go
+++ main.go
@@ -2,36 +2,34 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/hatimhas/gator_rss/internal/config"
 )
 
-//
-// func main() {
-// 	config, err := config.Read()
-// 	if err != nil {
-// 		fmt.Printf("Error opening config in main(): %v\n", err)
-// 		return
-// 	}
-// 	config.SetUser("hatim")
-//
-// 	fmt.Println(config.PrettyString())
-// }
-//
-//
-
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Error: not enough arguments provided.")
+		os.Exit(1)
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+	cmd := command{name: cmdName, arguments: cmdArgs}
+
 	cfg, err := config.Read()
 	if err != nil {
-		fmt.Printf("error reading config at scarth.go: %v", err)
+		fmt.Printf("error reading config : %v", err)
 		return
 	}
+
 	s := state{config: &cfg}
-	cmd := command{name: "login", arguments: []string{"harim"}}
 
-	if err := handlerLogin(&s, cmd); err != nil {
-		fmt.Printf("error handling Login at scarth.go: %v", err)
-		return
+	cmds := commands{cmdMap: make(map[string]func(*state, command) error)}
+	cmds.register("login", handlerLogin)
 
+	if err := cmds.run(&s, cmd); err != nil {
+		fmt.Fprintf(os.Stderr, "Error running command %s: %v\n", cmd.name, err)
+		os.Exit(1)
 	}
 }
--- utils.go
+++ utils.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/hatimhas/gator_rss/internal/config"
+import (
+	"fmt"
+
+	"github.com/hatimhas/gator_rss/internal/config"
+)
 
 type state struct {
 	config *config.Config
@@ -13,4 +17,23 @@ type command struct {
 
 type commands struct {
 	cmdMap map[string]func(*state, command) error
+}
+
+// - This method runs a given command with the provided state if it exists.
+func (c *commands) run(s *state, cmd command) error {
+	cmdFunc := c.cmdMap[cmd.name]
+	if cmdFunc == nil {
+		return fmt.Errorf("command %s not found", cmd.name)
+	}
+	// Passed cmd to cmdFunc to acess arguments
+	return cmdFunc(s, cmd)
+}
+
+// - This method registers a new handler function for a command name.
+func (c *commands) register(name string, f func(*state, command) error) error {
+	if _, exists := c.cmdMap[name]; exists {
+		return fmt.Errorf("Error: command already exists")
+	}
+	c.cmdMap[name] = f
+	return nil
 }
